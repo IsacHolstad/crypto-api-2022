101d1fa2190bd8074837103c420fd1f6535ed609
IsacHolstad <wisac22@gmail.com>
2022-03-17 21:21:57 +0100

    creating secound page

---
+++ cryptodetails.js
@@ -0,0 +1,32 @@
+const Apiurl = 'api.coincap.io/v2/assets/bitcoin'
+console.log(Apiurl)
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFixUrl = proxy + Apiurl;
+//console.log(corsFixUrl);
+//var key = "66579196-1262-493b-bde9-f1a036eea696"
+
+const cryptoContainer = document.querySelector(".container");
+
+async function cryptoNames() {
+    try{
+        const response = await fetch(corsFixUrl);
+        console.log(response.type);
+        const responseJSON = await response.json(); // convert the response to json data
+        console.log(responseJSON);
+        const cryptoData = responseJSON.data;
+        console.log(cryptoData)
+        for (let i = 0; i < cryptoData.length; i++) {
+            
+            console.log(cryptoData[i].data);
+            cryptoContainer.innerHTML += `<li><span><a href="detailcrypto.html?id=${cryptoData[i].symbol}">${cryptoData[i].symbol}</a></span></li>`;
+        }
+
+
+
+    }
+    catch(error) {
+        cryptoContainer.innerHTML += `<li>there is an error happening</li>`
+       
+    }
+}
+cryptoNames();
