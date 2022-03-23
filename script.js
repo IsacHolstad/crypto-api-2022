afd21f36a62ba7308e0c95409da3bbf41117ddcf
IsacHolstad <wisac22@gmail.com>
2022-03-23 10:14:21 +0100

    hotjr script added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2885261,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 const APIurl = 'api.coincap.io/v2/assets?key=66579196-1262-493b-bde9-f1a036eea696'
 const proxy = "https://noroffcors.herokuapp.com/";
 const corsFixUrl = proxy + APIurl;
